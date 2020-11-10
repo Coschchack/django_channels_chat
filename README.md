a036132415fcb837ac5ea8d0f92cefd395c29141
Coschchack <^6cda5Wa>
2020-11-10 16:46:50 +0100

    move to async consumer

--- README.md
+++ README.md
@@ -18,5 +18,9 @@ the WebSocket. Afterwards, the value of this field is cleared.
 In the log field, whenever a new message appears in the WebSocket, it's attached to that field.
 
 ### WebSocket 
-A basic consumer accepts WebSocket connections on the path /ws/chat/ROOM_NAME/ that takes any message it receives on 
-the WebSocket and echos it back to the same WebSocket.
+https://channels.readthedocs.io/en/stable/topics/channel_layers.html
+Each application instance (each open WebSocket) results in a single Consumer instance, and if you have channel layers enabled, Consumers will generate a unique channel name for themselves, and start listening on it for events.
+
+This means you can send those consumers events from outside the process - from other consumers, maybe, or from management commands - and they will react to them and run code just like they would events from their client connection.
+
+When a user posts a message, a JavaScript function will transmit the message over WebSocket to a ChatConsumer. The ChatConsumer will receive that message and forward it to the group corresponding to the room name. Every ChatConsumer in the same group (and thus in the same room) will then receive the message from the group and forward it over WebSocket back to JavaScript, where it will be appended to the chat log.
--- chat_room/consumers.py
+++ chat_room/consumers.py
@@ -1,22 +1,44 @@
 import json
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name,
+        )
+        # if we do not call accept() then the connection will be rejected and closed
+        await self.accept()
 
-    def disconnect(self, close_code):
-        pass
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name,
+        )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         """
-        Receive message and echo it back to the same client.
+        Receive message from a Websocket, and send it to the room group.
         """
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat.message",
+                "message": message,
+            }
+        )
 
-        self.send(text_data=json.dumps({
-            'message': message
+    async def chat_message(self, event):
+        """
+        When a message hits the room group, send it to the WebSocket.
+        """
+        message = event['message']
+        await self.send(text_data=json.dumps({
+            'message': message,
         }))
