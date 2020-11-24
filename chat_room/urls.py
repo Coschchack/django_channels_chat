f7996c6ba7c01995634179f1ba80fd9a7ac0bf3e
Coschchack <^6cda5Wa>
2020-11-24 17:26:33 +0100

    change the way redirections happen

--- chat_room/urls.py
+++ chat_room/urls.py
@@ -5,5 +5,5 @@ from chat_room.views import index_view, room_view
 
 urlpatterns = [
     path('', index_view, name='index'),
-    path('<str:room_name>/', room_view, name='room'),
+    path('room/', room_view, name='room'),
 ]
--- chat_room/views.py
+++ chat_room/views.py
@@ -1,23 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, HttpResponse, redirect
 
 from chat_room.forms import ChatForm
 
 
 def index_view(request):
+    return render(request, 'index.html', {'form': ChatForm()})
+
+
+def room_view(request):
     if request.method == 'POST':
         form = ChatForm(request.POST)
-        # If not valid, previously filled form will be presented in HTML
         if form.is_valid():
-            request.session['user_name'] = form.cleaned_data["user_name"]
-            return redirect(f'/chat/{form.cleaned_data["room_name"]}/')
+            context = {
+                "room_name": "ASDASDASD"
+            }
+            return render(request, 'room.html', context)
+        else:
+            return HttpResponse("Wrong data has been entered. Please try again", status=400)
     else:
-        form = ChatForm()
-
-    return render(request, 'index.html', {'form': form})
-
-
-def room_view(request, room_name):
-    context = {
-        "room_name": room_name
-    }
-    return render(request, 'room.html', context)
+        return redirect(index_view)
